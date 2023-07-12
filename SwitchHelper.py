87d933603ea9025819d39e6c8f6c17cb11947dac
Cristiano Arnaudo <skyocrandive@gmail.com>
2023-07-12 16:38:33 +0200

    realizzato SwitchHelper

--- SwitchHelper.py
+++ SwitchHelper.py
@@ -1,174 +1,138 @@
-#=============================================================================
+# =============================================================================
 # Decide whether the IA should switch Pokémon
-#=============================================================================
+# =============================================================================
+import random
 
 import MoveUtilities
 
-from poke_env.environment import Move, Pokemon, DoubleBattle, Effect, Status, Weather, SideCondition
-from poke_env.environment.move_category import MoveCategory
-from poke_env.environment.pokemon_type import PokemonType
+from poke_env.environment import Move, Pokemon, DoubleBattle, Effect
 
-def should_withdraw(battle : DoubleBattle, idxBattler):
 
-    shouldSwitch = False
-    batonPass = -1
-    moveType = None
+def should_withdraw(battle: DoubleBattle, idxBattler) -> Pokemon | None:
+    """
+    return the best switch if pokemon identified by idxBattler should switch, else return None
+    :param battle:
+    :param idxBattler:
+    :return:
+    """
+
+    ai_random = random
+    ai_random.randint(0, 100)
+    should_switch = False
+
     battler = battle.active_pokemon[idxBattler]
-    # If Pokémon is within 6 levels of the foe, and foe's last move was
-    # super-effective and powerful
-    target = battle.opponent_active_pokemon[idxBattler]
-    if not target.fainted and target.lastMoveUsed:
-        moveData = GameData::Move.get(target.lastMoveUsed)
-        moveType = moveData.type
-        typeMod = pbCalcTypeMod(moveType, target, battler)
-        if Effectiveness.super_effective(typeMod) and moveData.base_damage > 50:
-          switchChance = (moveData.base_damage > 70) ? 30 : 20
-          shouldSwitch = (pbAIRandom(100) < switchChance)
+    predictions = []
 
+    # If foe's move can  be super-effective and powerful
+    for target in battle.opponent_active_pokemon:
+        # predicted_move = Move
+        (predicted_move, predicted_damage) = MoveUtilities.get_opponent_max_damage_move(battle, battler, target)
+        predictions.append((target, predicted_move, predicted_damage))
+
+        if MoveUtilities.can_outspeed(battle, target, battler) and battler.damage_multiplier(predicted_move.type) > 1:
+            switchChance = 0
+            if predicted_damage >= 98:  # most certain ohko
+                switchChance = 80
+            elif predicted_damage > 70:  # does a lot of damage
+                switchChance = 30
+            should_switch = ai_random.randint(0, 100) < switchChance
 
     # Pokémon can't do anything
     if battle.available_moves[idxBattler][0] == Move.retrieve_id("struggle"):
-        shouldSwitch = True
+        should_switch = True
 
-    # Pokémon is Perish Songed and has Baton Pass
-    if skill >= PBTrainerAI.highSkill && battler.effects[PBEffects::PerishSong] == 1
-      battler.eachMoveWithIndex do |m, i|
-        next if m.function != "SwitchOutUserPassOnEffects"   # Baton Pass
-        next if !@battle.pbCanChooseMove?(idxBattler, i, False)
-        batonPass = i
-        break
-      end
-    end
-    # Pokémon will faint because of bad poisoning at the end of this round, but
-    # would survive at least one more round if it were regular poisoning instead
-    if battler.status == :POISON && battler.statusCount > 0 &&
-       skill >= PBTrainerAI.highSkill
-      toxicHP = battler.totalhp / 16
-      nextToxicHP = toxicHP * (battler.effects[PBEffects::Toxic] + 1)
-      if battler.hp <= nextToxicHP && battler.hp > toxicHP * 2 && pbAIRandom(100) < 80
-        shouldSwitch = True
-      end
-    end
     # Pokémon is Encored into an unfavourable move
-    if battler.effects[PBEffects::Encore] > 0 && skill >= PBTrainerAI.mediumSkill
-      idxEncoredMove = battler.pbEncoredMoveIndex
-      if idxEncoredMove >= 0
-        scoreSum   = 0
-        scoreCount = 0
-        battler.allOpposing.each do |b|
-          scoreSum += pbGetMoveScore(battler.moves[idxEncoredMove], battler, b, skill)
-          scoreCount += 1
-        end
-        if scoreCount > 0 && scoreSum / scoreCount <= 20 && pbAIRandom(100) < 80
-          shouldSwitch = True
-        end
-      end
-    end
+    if battler.effects.get(Effect.ENCORE) is not None:
+        move = battle.available_moves[idxBattler][0]
+        score_sum = 0
+        score_count = 0
+        for target in battle.opponent_active_pokemon:
+            score_sum += pbGetMoveScore(move, battler, target, skill)
+            score_count += 1
+        if score_count > 0 and score_sum / score_count <= 20 and ai_random.randint(0, 100) < 80:
+            should_switch = True
+
     # If there is a single foe and it is resting after Hyper Beam or is
     # Truanting (i.e. free turn)
-    if @battle.pbSideSize(battler.index + 1) == 1 &&
-       !battler.pbDirectOpposing.fainted? && skill >= PBTrainerAI.highSkill
-      opp = battler.pbDirectOpposing
-      if (opp.effects[PBEffects::HyperBeam] > 0 ||
-         (opp.hasActiveAbility?(:TRUANT) && opp.effects[PBEffects::Truant])) && pbAIRandom(100) < 80
-        shouldSwitch = False
-      end
-    end
-    # Sudden Death rule - I'm not sure what this means
-    if @battle.rules["suddendeath"] && battler.turnCount > 0
-      if battler.hp <= battler.totalhp / 4 && pbAIRandom(100) < 30
-        shouldSwitch = True
-      elsif battler.hp <= battler.totalhp / 2 && pbAIRandom(100) < 80
-        shouldSwitch = True
-      end
-    end
+    if len(battle.opponent_active_pokemon) == 1:
+        opp = battle.opponent_active_pokemon[0]
+        if opp.must_recharge and ai_random.randint(0, 100) < 80:
+            should_switch = False
+
     # Pokémon is about to faint because of Perish Song
-    if battler.effects[PBEffects::PerishSong] == 1
-      shouldSwitch = True
-    end
-    if shouldSwitch
-      list = []
-      idxPartyStart, idxPartyEnd = @battle.pbTeamIndexRangeFromBattlerIndex(idxBattler)
-      @battle.pbParty(idxBattler).each_with_index do |pkmn, i|
-        next if i == idxPartyEnd - 1   # Don't choose to switch in ace
-        next if !@battle.pbCanSwitch?(idxBattler, i)
-        # If perish count is 1, it may be worth it to switch
-        # even with Spikes, since Perish Song's effect will end
-        if battler.effects[PBEffects::PerishSong] != 1
-          # Will contain effects that recommend against switching
-          spikes = battler.pbOwnSide.effects[PBEffects::Spikes]
-          # Don't switch to this if too little HP
-          if spikes > 0
-            spikesDmg = [8, 6, 4][spikes - 1]
-            next if pkmn.hp <= pkmn.totalhp / spikesDmg &&
-                    !pkmn.hasType?(:FLYING) && !pkmn.hasActiveAbility?(:LEVITATE)
-          end
-        end
-        # moveType is the type of the target's last used move
-        if moveType && Effectiveness.ineffective?(pbCalcTypeMod(moveType, battler, battler))
-          weight = 65
-          typeMod = pbCalcTypeModPokemon(pkmn, battler.pbDirectOpposing(True))
-          if Effectiveness.super_effective?(typeMod)
-            # Greater weight if new Pokemon's type is effective against target
-            weight = 85
-          end
-          list.unshift(i) if pbAIRandom(100) < weight   # Put this Pokemon first
-        elsif moveType && Effectiveness.resistant?(pbCalcTypeMod(moveType, battler, battler))
-          weight = 40
-          typeMod = pbCalcTypeModPokemon(pkmn, battler.pbDirectOpposing(True))
-          if Effectiveness.super_effective?(typeMod)
-            # Greater weight if new Pokemon's type is effective against target
-            weight = 60
-          end
-          list.unshift(i) if pbAIRandom(100) < weight   # Put this Pokemon first
-        else
-          list.push(i)   # put this Pokemon last
-        end
-      end
-      if list.length > 0
-        if batonPass >= 0 && @battle.pbRegisterMove(idxBattler, batonPass, False)
-          PBDebug.log("[AI] #{battler.pbThis} (#{idxBattler}) will use Baton Pass to avoid Perish Song")
-          return True
-        end
-        if @battle.pbRegisterSwitch(idxBattler, list[0])
-          PBDebug.log("[AI] #{battler.pbThis} (#{idxBattler}) will switch with " +
-                      @battle.pbParty(idxBattler)[list[0]].name)
-          return True
-        end
-      end
-    end
-    return False
+    if battler.effects.get(Effect.PERISH1) is not None:
+        should_switch = True
+
+    if should_switch:
+        switch_order = []
+        for pkmn in battle.available_switches[idxBattler]:
+            weight = 0
+            for (target, move, damage) in predictions:
+                move_type = move.type
+                # moveType is the type of the predicted move that will hit battler
+                type_mod = pkmn.damage_multiplier(move_type)
+                temp_weight = 0
+                if type_mod == 0:  # if switch immune
+                    temp_weight = 65
+                    if MoveUtilities.can_damage(pkmn, target):
+                        # Greater weight if new Pokemon's can hit effectively the target
+                        temp_weight = 85
+                elif type_mod < 1:  # if switch resist
+                    temp_weight = 40
+                    if MoveUtilities.can_damage(pkmn, target):
+                        # Greater weight if new Pokemon's can hit effectively the target
+                        temp_weight = 60
+                if temp_weight > weight:
+                    weight = temp_weight
+
+            if ai_random.randint(0, 100) < weight:
+                switch_order.insert(0, pkmn)  # Put this Pokemon first
+
+            else:
+                switch_order.append(pkmn)  # put this Pokemon last
+
+        if len(switch_order) > 0:
+            return switch_order[0]
+    return None
 
 
-#=============================================================================
+# =============================================================================
 # Choose a replacement Pokémon
-#=============================================================================
-def pbDefaultChooseNewEnemy(idxBattler, party):
-    enemies = []
-    idxPartyStart, idxPartyEnd = @battle.pbTeamIndexRangeFromBattlerIndex(idxBattler)
-    party.each_with_index do |_p, i|
-      next if i == idxPartyEnd - 1 && enemies.length > 0   # Ignore ace if possible
-      enemies.push(i) if @battle.pbCanSwitchLax?(idxBattler, i)
-    end
-    return -1 if enemies.length == 0
-    return pbChooseBestNewEnemy(idxBattler, party, enemies)
+# =============================================================================
+
+def choose_possible_best_switch(battle, index: int):
+    if not battle.available_switches:
+        return None
+    # Go through each Pokémon that can be switched to, and choose one with the best type matchup against both opponents
+    # (smaller multipliers are better)
+    best_score = float('inf')
+    best_switch = battle.available_switches[index][0]
+    for switch in battle.available_switches[index]:
+        score1 = get_matchup_score_default(battle, switch, battle.opponent_active_pokemon[0])
+        score2 = get_matchup_score_default(battle, switch, battle.opponent_active_pokemon[1])
+        score = score1 + score2
+        if score < best_score:
+            best_score = score
+            best_switch = switch
+    return best_switch
 
 
-def pbChooseBestNewEnemy(idxBattler, party, enemies):
-    if not enemies or enemies.length == 0:
-        return -1
-    best    = -1
-    bestSum = 0
-    for i in enemies:
-      pkmn = party[i]
-      sum  = 0
-      for m in pkmn.moves:
-        if m.base_damage == 0:
-            continue
-        for b in @battle.battlers[idxBattler].allOpposing:
-          bTypes = b.pbTypes(True)
-          sum += Effectiveness.calculate(m.type, bTypes[0], bTypes[1], bTypes[2])
-      if best == -1 or sum > bestSum
-        best = i
-        bestSum = sum
-    return best
+def get_matchup_score_default(battle: DoubleBattle, my_pokemon: Pokemon, opponent_pokemon: Pokemon):
+    score = 0
+    if opponent_pokemon is None:
+        return score
+    defensive_multiplier = MoveUtilities.pokemon_type_advantage(my_pokemon, opponent_pokemon)
+    # A multiplier greater than 1 means we are at a type disadvantage. If there is a better type match, switch
+    if defensive_multiplier == 4:
+        score += 1
+    elif defensive_multiplier == 2:
+        score += 0.5
+    elif defensive_multiplier == 0.5:
+        score -= 0.5
+    elif defensive_multiplier == 0.25:
+        score -= 1
+    if MoveUtilities.can_outspeed(battle, my_pokemon, opponent_pokemon):
+        score += 0.5
+    if MoveUtilities.can_damage(my_pokemon, opponent_pokemon):
+        score += 0.5
+    return score
